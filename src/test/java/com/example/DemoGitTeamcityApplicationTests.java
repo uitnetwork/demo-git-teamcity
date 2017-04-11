5070dade91def497260cd441522bae112cdb8fd3
Ninh Doan <ninh@m-daq.com>
2017-04-11 18:10:46 +0800

    Change propreties

--- src/test/java/com/example/DemoGitTeamcityApplicationTests.java
+++ src/test/java/com/example/DemoGitTeamcityApplicationTests.java
@@ -1,16 +1,18 @@
 package com.example;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class DemoGitTeamcityApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
